c2db7a2189d1ab06642adf3bbc1d51148ce61d5a
Caio Henrique <cahenromar@gmail.com>
2026-02-04 15:36:57 -0300

    chore(properties): scaffold property form and details pages

---
+++ src/modules/properties/pages/PropertyDetailsPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-card elevation="2">
+      <v-card-title class="pa-4">
+        <v-icon class="mr-3" color="primary">mdi-home</v-icon>
+        Detalhes do Imóvel
+      </v-card-title>
+      <v-card-text class="pa-4">
+        <p class="text-body-1 text-medium-emphasis">
+          Página de detalhes será implementada em breve.
+        </p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+// PropertyDetailsPage component
+</script>
+
---
+++ src/modules/properties/pages/PropertyFormPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-card elevation="2">
+      <v-card-title class="pa-4">
+        <v-icon class="mr-3" color="primary">mdi-home-plus</v-icon>
+        {{ isEditMode ? 'Editar Imóvel' : 'Novo Imóvel' }}
+      </v-card-title>
+      <v-card-text class="pa-4">
+        <p class="text-body-1 text-medium-emphasis">
+          Formulário de imóvel será implementado em breve.
+        </p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const isEditMode = computed(() => !!route.params.id)
+</script>
+
